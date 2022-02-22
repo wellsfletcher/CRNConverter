be5c9177a375c7175d5455ff65159c9e91018f73
wellsfletcher <wellsfletcher@gmail.com>
2022-02-21 22:09:11 -0500

    Initial Commit

---
+++ ClassmateConnector/ClassmateConnectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassmateConnectorApp.swift
+//  ClassmateConnector
+//
+//  Created by Fletcher Wells on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ClassmateConnectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassmateConnector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClassmateConnector
+//
+//  Created by Fletcher Wells on 2/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
